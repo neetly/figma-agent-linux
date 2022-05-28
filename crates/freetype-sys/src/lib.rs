fc05c1a8f78d7358234c18d239f6ea4d631bf44b
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-28 12:01:09 +0800

    Update

--- crates/freetype-sys/src/lib.rs
+++ crates/freetype-sys/src/lib.rs
@@ -1,15 +1,32 @@
 #![allow(non_camel_case_types, non_upper_case_globals)]
 
 use ffi_utils::opaque;
-use libc::c_int;
+use libc::{c_int, c_long, c_void};
+
+pub type FT_Library = *mut FT_LibraryRec;
+pub type FT_Face = *mut FT_FaceRec;
 
 opaque! {
     #[repr(C)]
-    pub struct FT_Library;
+    pub struct FT_LibraryRec;
 
     #[repr(C)]
-    pub struct FT_Face;
+    pub struct FT_FaceRec;
 }
+
+pub type FT_Memory = *mut FT_MemoryRec;
+
+#[repr(C)]
+pub struct FT_MemoryRec {
+    pub user: *mut c_void,
+    pub alloc: FT_Alloc_Func,
+    pub realloc: FT_Realloc_Func,
+    pub free: FT_Free_Func,
+}
+
+pub type FT_Alloc_Func = extern "C" fn(FT_Memory, c_long) -> *mut c_void;
+pub type FT_Realloc_Func = extern "C" fn(FT_Memory, c_long, c_long, *mut c_void) -> *mut c_void;
+pub type FT_Free_Func = extern "C" fn(FT_Memory, *mut c_void);
 
 pub type FT_Error = c_int;
 
@@ -18,4 +35,10 @@ pub const FT_Err_Ok: FT_Error = 0x00;
 #[link(name = "freetype")]
 extern "C" {
     pub fn FT_Init_FreeType(library: *mut FT_Library) -> FT_Error;
+
+    pub fn FT_New_Library(memory: FT_Memory, library: *mut FT_Library) -> FT_Error;
+    pub fn FT_Reference_Library(library: FT_Library) -> FT_Error;
+    pub fn FT_Done_Library(library: FT_Library) -> FT_Error;
+
+    pub fn FT_Add_Default_Modules(library: FT_Library) -> FT_Error;
 }
--- crates/freetype/src/lib.rs
+++ crates/freetype/src/lib.rs
@@ -1,1 +1,76 @@
-pub use freetype_sys;
+#![allow(non_upper_case_globals, clippy::missing_safety_doc)]
+
+use std::ptr;
+
+use libc::{c_long, c_void, free, malloc, realloc};
+
+pub use freetype_sys::*;
+
+#[macro_export]
+macro_rules! dispatch {
+    ($expr:expr) => {
+        dispatch!($expr, ())
+    };
+
+    ($expr:expr, $result:expr) => {{
+        let result = $expr;
+        match result {
+            FT_Err_Ok => Ok($result),
+            _ => Err(result),
+        }
+    }};
+}
+
+pub struct Library {
+    raw: FT_Library,
+}
+
+impl Library {
+    pub fn new() -> Result<Library, FT_Error> {
+        let mut raw: FT_Library = ptr::null_mut();
+        unsafe { dispatch!(FT_New_Library(&mut MEMORY, &mut raw), Library { raw }) }
+    }
+
+    pub unsafe fn from_raw(raw: FT_Library) -> Result<Library, FT_Error> {
+        Ok(Library { raw })
+    }
+
+    pub unsafe fn from_raw_with_ref(raw: FT_Library) -> Result<Library, FT_Error> {
+        dispatch!(FT_Reference_Library(raw), Library { raw })
+    }
+
+    pub fn init() -> Result<Library, FT_Error> {
+        let library = Library::new()?;
+        unsafe { dispatch!(FT_Add_Default_Modules(library.raw), library) }
+    }
+}
+
+impl Drop for Library {
+    fn drop(&mut self) {
+        unsafe { FT_Done_Library(self.raw) };
+    }
+}
+
+pub static mut MEMORY: FT_MemoryRec = FT_MemoryRec {
+    user: ptr::null_mut(),
+    alloc: memory_alloc,
+    realloc: memory_realloc,
+    free: memory_free,
+};
+
+extern "C" fn memory_alloc(_: FT_Memory, size: c_long) -> *mut c_void {
+    unsafe { malloc(size as usize) }
+}
+
+extern "C" fn memory_realloc(
+    _: FT_Memory,
+    _: c_long,
+    size: c_long,
+    pointer: *mut c_void,
+) -> *mut c_void {
+    unsafe { realloc(pointer, size as usize) }
+}
+
+extern "C" fn memory_free(_: FT_Memory, pointer: *mut c_void) {
+    unsafe { free(pointer) }
+}
