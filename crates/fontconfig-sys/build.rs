7bf308c355ab331cee0d138336554fef08aaff94
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-29 16:31:15 +0800

    Add bindgen

---
+++ crates/fontconfig-sys/build.rs
@@ -0,0 +1,26 @@
+use std::{env, path::PathBuf};
+
+use bindgen::{builder, CargoCallbacks, MacroTypeVariation};
+use pkg_config::probe_library;
+
+fn main() {
+    println!("cargo:rerun-if-changed=./include/fontconfig.h");
+
+    let library = probe_library("fontconfig").unwrap();
+    let clang_args: Vec<_> = library
+        .include_paths
+        .iter()
+        .map(|path| format!("-I{}", path.display()))
+        .collect();
+
+    let bindings = builder()
+        .header("./include/fontconfig.h")
+        .clang_args(clang_args)
+        .parse_callbacks(Box::new(CargoCallbacks))
+        .default_macro_constant_type(MacroTypeVariation::Signed)
+        .generate()
+        .unwrap();
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings.write_to_file(out_dir.join("bindings.rs")).unwrap();
+}
