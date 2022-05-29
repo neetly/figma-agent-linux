b62a6aaff095e6ab8f32b9cd05594cccebc5f5f1
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-30 00:52:16 +0800

    Fix ci

--- crates/freetype-sys/include/freetype.h
+++ crates/freetype-sys/include/freetype.h
@@ -1,8 +1,10 @@
+#include <ft2build.h>
+
 #define FT_INCLUDE_ERR_PROTOS
 #define FT_ERRORDEF(e, v, s) const int e = v;
 #define FT_MODERRDEF(e, v, s) const int FT_Mod_Err_##e = v;
 
-#include <freetype/freetype.h>
-#include <freetype/ftmm.h>
-#include <freetype/ftmodapi.h>
-#include <freetype/ftsnames.h>
+#include FT_FREETYPE_H
+#include FT_MODULE_H
+#include FT_SFNT_NAMES_H
+#include FT_MULTIPLE_MASTERS_H
