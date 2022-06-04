42fc9672005da008fb748ed22e0674c789a11364
Hikari Hayashi <rev.hikari@gmail.com>
2022-06-04 20:58:54 +0800

    Update

--- crates/freetype/src/sfnt_name.rs
+++ crates/freetype/src/sfnt_name.rs
@@ -1,1 +1,28 @@
-pub struct SfntName {}
+#![allow(clippy::missing_safety_doc)]
+
+use std::mem;
+
+use freetype_sys::FT_SfntName;
+
+pub struct SfntName {
+    raw: FT_SfntName,
+}
+
+impl AsRef<FT_SfntName> for SfntName {
+    fn as_ref(&self) -> &FT_SfntName {
+        &self.raw
+    }
+}
+
+impl AsMut<FT_SfntName> for SfntName {
+    fn as_mut(&mut self) -> &mut FT_SfntName {
+        &mut self.raw
+    }
+}
+
+impl SfntName {
+    pub unsafe fn new() -> SfntName {
+        let raw = mem::zeroed();
+        SfntName { raw }
+    }
+}
