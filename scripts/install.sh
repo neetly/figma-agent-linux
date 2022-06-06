f9dc61cc5c94bd0366f6e72eb6de10196e2487fd
Hikari Hayashi <rev.hikari@gmail.com>
2022-06-07 01:44:05 +0800

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eo pipefail
+
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+echo ":: Figma Agent for Linux"
+echo ":: ====================="
+
+if systemctl --user is-active figma-agent.service > /dev/null 2>&1; then
+  echo ":: Stopping figma-agent.service"
+  systemctl --user stop figma-agent.service
+fi
+
+echo ":: Downloading figma-agent"
+mkdir -p "$XDG_DATA_HOME/figma-agent"
+curl --location https://github.com/neetly/figma-agent-linux/releases/latest/download/figma-agent-x86_64-unknown-linux-gnu \
+  --output "$XDG_DATA_HOME/figma-agent/figma-agent" --fail
+chmod +x "$XDG_DATA_HOME/figma-agent/figma-agent"
+
+echo ":: Writing to figma-agent.service"
+mkdir -p "$XDG_CONFIG_HOME/systemd/user"
+cat > "$XDG_CONFIG_HOME/systemd/user/figma-agent.service" << EOF
+[Unit]
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=exec
+ExecStart="$XDG_DATA_HOME/figma-agent/figma-agent"
+
+[Install]
+WantedBy=default.target
+EOF
+
+echo ":: Enabling figma-agent.service"
+systemctl --user daemon-reload
+systemctl --user enable --now figma-agent.service
+
+echo ":: Done"
